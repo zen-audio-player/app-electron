da8e8f0ed750d4df01de6cc3644758d46b63160c
Iakov Boyko <selfishman@Iakovs-MBP.home>
2016-07-21 21:34:40 -0400

    Reconfiguring

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 // Module to control application life.
 const app = electron;
 // Module to create native browser window.
-const {BrowserWindow} = electron;
+const BrowserWindow = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
